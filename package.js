e6daed910b34e9b77464ab3032f0291e5a428ef8
iumaralamin <iumaralamin@gmail.com>
2026-01-05 11:47:43 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "mega-file-server",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.5",
+    "express": "^4.18.2",
+    "megajs": "^1.1.0",
+    "multer": "^2.0.0-rc.8"
+  }
+}
