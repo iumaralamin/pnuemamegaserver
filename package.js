24524ecb5d7517e87cdf138922a3d85b1f484a27
iumaralamin <iumaralamin@gmail.com>
2026-01-05 12:02:19 +0100

    Update package.js

--- package.js
+++ package.js
@@ -6,9 +6,9 @@
     "start": "node index.js"
   },
   "dependencies": {
+    "express": "^4.18.2",
     "cors": "^2.8.5",
     "dotenv": "^16.4.5",
-    "express": "^4.18.2",
     "megajs": "^1.1.0",
     "multer": "^2.0.0-rc.8"
   }
